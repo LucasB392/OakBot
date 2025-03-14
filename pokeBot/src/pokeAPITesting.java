d4437ab0eda97d5102201895cb885c8f8eb91bbc
Lucas Nathaniel Cedrick Brown <lbrow93@uwo.ca>
2025-03-14 11:11:25 -0400

    Testing/Initial commit.

---
+++ pokeBot/src/pokeAPITesting.java
@@ -0,0 +1,28 @@
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class pokeAPITesting {
+
+    public static void main(String[] args) {
+        // Create OkHttpClient instance
+        OkHttpClient client = new OkHttpClient();
+        System.out.println("hi");
+
+        // Pokémon API URL (e.g., Pikachu)
+        String url = "https://pokeapi.co/api/v2/pokemon/pikachu";
+
+        // Create the request
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        // Send the request and handle the response
+        try (Response response = client.newCall(request).execute()) {
+            // Print out the response body
+            System.out.println(response.body().string());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
